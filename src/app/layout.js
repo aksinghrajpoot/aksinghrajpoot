4ca9bbfdb67c188d8e171d1008b825dd16312eca
aksinghrajpoot <imaksinghrajpoot@gmail.com>
2024-12-16 21:52:26 +0530

    v1

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,24 +1,28 @@
 import Header from "@/components/Header/Header";
 import "@/styles/global.css";
 
-
 export const metadata = {
   title: {
-    template: '%s - A K Singh',
-    default: 'A K Singh', 
+    template: "%s - A K Singh",
+    default: "A K Singh",
   },
-  description:'Hello! My name is Ashok and I am a software developer located in India, I have a serious passion for web devlopment, designs and interactive user experiences.',
-}
+  description:
+    "Hello! My name is Ashok and I am a software developer located in India, I have a serious passion for web devlopment, designs and interactive user experiences.",
+  };
+
+  export const viewport = {
+    themeColor:'#155e75', 
+  };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body>
-      <Header />
-        {children}
+     
 
-       
-        </body>
+      <body>
+        <Header />
+        {children}
+      </body>
     </html>
   );
 }
