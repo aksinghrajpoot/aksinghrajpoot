50cc42d442abdee2e2f2f1624c26aeb46d00d3f7
aksinghrajpoot <imaksinghrajpoot@gmail.com>
2025-03-21 20:47:22 +0530

    'v1

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -7,18 +7,17 @@ export default function Aboutcomponent() {
       <section className="about_me" id="about-me">
         <div className="container">
           <h2 className="numberedHeading">About Me</h2>
-
           <p className="font-poppins mt-6">
             Hello! I&apos;m <span className="text-primary font-semibold">Ashok</span>, a passionate  
             <span className="text-primary font-semibold"> Software Developer & UI/UX Enthusiast</span> based in India.  
             I have a deep interest in web development, modern UI/UX design, and crafting seamless digital experiences.  
-            I also enjoy **writing tech blogs and sharing knowledge** with the developer community.
+            I also enjoy writing tech blogs and sharing knowledge with the developer community.
           </p>
 
           <p className="mt-4">
-            I specialize in designing and developing **high-performance, responsive, and interactive websites**.  
-            My expertise includes **React.js, Next.js, Angular, and cloud technologies**.  
-            If you're looking for a **modern and scalable solution**, feel free to **reach out to me**!
+            I specialize in designing and developing **high-performance, responsive, and interactive websites.  
+            My expertise includes React.js, Next.js, Angular, and cloud technologies.  
+            If you&apos;re looking for a modern and scalable solution, feel free to reach out to me!
           </p>
 
           <p className="mt-4">
