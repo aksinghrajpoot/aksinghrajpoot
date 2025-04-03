1e93755e8012bcaa7820696785b4f120372ed6a6
aksinghrajpoot <imaksinghrajpoot@gmail.com>
2025-04-03 12:28:52 +0530

    v1

--- src/app/about/page.js
+++ src/app/about/page.js
@@ -12,7 +12,7 @@ export default function About() {
           <h1 className="text-primary">About Us</h1>
           <hr className="border-t-2 border-primary w-[70px] mt-3" />
           <p className="mt-4">
-            Hello! I'm Ashok Singh, a passionate Front-end Developer, UI/UX
+            Hello! I&apos;m Ashok Singh, a passionate Front-end Developer, UI/UX
             Enthusiast, and Software Engineer from India. With a strong
             foundation in web development, modern UI/UX design, and digital
             product strategy, I craft seamless, high-performance experiences for
